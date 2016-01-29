1bdb1f729d37e659dd176d00ac3c0fa3eb4cb140
ddolcimascolo <ddolcimascolo@linagora.com>
2016-01-29 11:11:58 +0100

    Initial version of the website.

---
+++ lang/en.js
@@ -0,0 +1,22 @@
+var languages = languages || {};
+
+languages.en = {
+  TITLE: 'OpenPaaS',
+  MAIN_TITLE: 'OpenPaaS virtual desktop',
+  MAIN_TAGLINE: 'Professional collaboration made simple',
+  PRESENTATION_TITLE: 'Presentation',
+  PRESENTATION_CONTENTS: 'Yolo <strong>hey !</strong> and more',
+  FONCTIONNALITES_TITLE: 'Features',
+  FONCTIONNALITES_CONTENTS: 'Welcome to the OpenPaas Features',
+  FONCTIONNALITES_H2: 'Édition collaborative temps réel',
+  FONCTIONNALITES_H3: 'Éditez vos textes, feuilles de calcul et présentations simultanément',
+  FONCTIONNALITES_P1: 'Travaillez à plusieurs et sur le même document en même temps : les modifications sont appliquées et affichées en temps réel à chacun des participants. OpenPaaS::NG indique les contributeurs actuellement en édition ainsi que leurs activités courantes.',
+  FONCTIONNALITES_P2: 'Il est alors aisé de collaborer sur le même document et de constater en temps réel les ajouts ou modifications et qui les effectuent. Éditeur de texte, de présentation ou feuille de calcul : toute la suite bureautique OpenPaaS::NG bénéficie de ces avantages.',
+  TECHNOLOGIES_TITLE: 'Technologies',
+  TECHNOLOGIES_CONTENT: 'Here are some of our technical challenges',
+  APROPOS_TITLE: 'About us',
+  APROPOS_CONTENT: 'The team OpenPaas is composed of 10 persons',
+  VIDEOS_TITLE: 'Latest Videos',
+  BLOG_TITLE: 'Blog',
+  LATEST_VIDEOS: 'Latest Videos'
+};
